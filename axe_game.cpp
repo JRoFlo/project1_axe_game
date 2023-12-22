f033af74f2a872fce022788c3c031a98d0203521
Jordan Oliver <bus.jordan.r.oliver@gmail.com>
2023-12-22 13:46:49 -0800

    GameDev - Moving the Circle

--- axe_game.cpp
+++ axe_game.cpp
@@ -12,19 +12,38 @@
 
 int main()
 {
-    //Variables
+    //Window Dimensions
     int width{1280};
-    int length{720};
+    int height{720};
  
-    //InitWindow takes three parameters - InitWindow(int width, int height, string title)
-    InitWindow(width, length, "Jordan's Window"); //This function will create a window with the dimensions 1280px x 720px
+    //Circle Coordinates
+    int circle_x{640};
+    int circle_y{360};
+    int circle_rad{25};
 
+    //InitWindow takes three parameters - InitWindow(int width, int height, string title)
+    InitWindow(width, height, "Jordan's Window"); //This function will create a window with the dimensions 1280px x 720px
+
+
+    //Set a Target Value for FPS so the computer doesnt update so many times
+    SetTargetFPS(60);
     //Infinite While Loop to keep the canvas on screen and drawing properly.
     while(WindowShouldClose() != true) //or while(WindowShouldClose() == false)
     {
         BeginDrawing(); //Set up the canvas
-        DrawCircle(640, 360, 25.0, RED);
         ClearBackground(PURPLE); //Clear the background with a color to prevent flickering
+
+        //Game Logic Begins
+        DrawCircle(circle_x, circle_y, circle_rad, RED);
+        if(IsKeyDown(KEY_D))
+            circle_x = circle_x + 2;
+        if(IsKeyDown(KEY_A))
+            circle_x = circle_x - 2;
+        if(IsKeyDown(KEY_W))
+            circle_y = circle_y - 2;
+        if(IsKeyDown(KEY_S))
+            circle_y = circle_y + 2;
+        //Game Logic Ends
         EndDrawing();
     }
 
@@ -38,4 +57,5 @@ int main()
         EndDrawing(); - Teardown
         ClearBackground(COLOR PARAMETER); - Clears the Canvas
         WindowShouldClose() - This function returns a false bool, UNLESS we hit the escape key or the X (exit button)
+        IsKeyDown(KEY_D) - Checks to see if the key D is being pressed
 */
