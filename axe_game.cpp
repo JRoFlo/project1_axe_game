7cf670917358b5c3a2179883e110a538ae9467d4
Jordan Oliver <bus.jordan.r.oliver@gmail.com>
2023-12-22 13:24:01 -0800

    GameDev - Clear Background

--- axe_game.cpp
+++ axe_game.cpp
@@ -15,26 +15,25 @@ int main()
     //Variables
     int width{1280};
     int length{720};
-
-    //Loop Variables
-    int miles_to_disney{100};
-
-    //While Loop to check if we are getting closer to Disney Land
-    while(miles_to_disney > 0)
-    {
-        std::cout << "Are we there yet?" << std::endl;
-        miles_to_disney = miles_to_disney - 1;
-    }
-    if(miles_to_disney == 0) 
-        std::cout << "Arrived at destination." << std::endl;
-
+ 
     //InitWindow takes three parameters - InitWindow(int width, int height, string title)
     InitWindow(width, length, "Jordan's Window"); //This function will create a window with the dimensions 1280px x 720px
+
+    //Infinite While Loop to keep the canvas on screen and drawing properly.
+    while(true)
+    {
+        BeginDrawing(); //Set up the canvas
+        ClearBackground(PURPLE); //Clear the background with a color to prevent flickering
+        EndDrawing();
+    }
 
     //Reaching the end of main function
     return 0;
 }
 
 /*
-    Loops: 
+    RayLib Functions:
+        BeginDrawing(); - Setup
+        EndDrawing(); - Teardown
+        ClearBackground(COLOR PARAMETER); - Clears the Canvas
 */
